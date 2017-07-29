fb4862248e4fce26e93c17e1693932f1cd6cf56d
AlvarBer <alvaro.garcia95@hotmail.com>
2017-07-29 20:44:16 +0100

    Refactored Fire

--- Scenes/FireManager.gd
+++ Scenes/FireManager.gd
@@ -1,20 +1,34 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var animator
+var since_last_kindle
+var animations = ["Low Fire", "Medium Fire", "High Fire", "Max Fire"]
+var timeouts = [60, 40, 30, 15]
 
 func _ready():
 	animator = get_node("AnimationPlayer")
-	animator.play("Low Fire")
+	animator.play(animations[0])
+	set_process(true)
+	since_last_kindle = 0
+
+func _process(delta):
+	since_last_kindle += delta
 
 func kindle():
-	animator.play("Medium Fire")
+	var idx = animations.find(animator.get_current_animation())
+	if idx + 1 < animations.size():
+		animator.play(animations[idx + 1])
+	else:
+		print("Fire at max")
+	since_last_kindle = 0
+
+func unkindle():
+	if (since_last_kindle >= 20 and
+	    animator.get_current_animation() == "Max Fire"):
+		animator.play("High Fire")
+	
 
 # Just for testing
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed):
-		print('Kindeling')
-		animator.stop()
 		kindle()
