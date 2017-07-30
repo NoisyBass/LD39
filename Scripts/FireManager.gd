cb90836953906f351129358813ea6ccd5796e74c
AlvarBer <alvaro.garcia95@hotmail.com>
2017-07-30 14:00:19 +0100

    Small refactor

--- Scripts/FireManager.gd
+++ Scripts/FireManager.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 export(Texture) var day_fire
-export var night_fire_anims = ["Low Fire", "Medium Fire",
+export var night_anims = ["Low Fire", "Medium Fire",
                                "High Fire", "Max Fire"]
-export var night_fire_timeouts = [60, 40, 30, 15]
+export var night_timeouts = [60, 40, 30, 15]
 
 onready var animator = get_node("AnimationPlayer")
 onready var fire_sprite = get_node("Sprite")
@@ -21,28 +21,26 @@ func _process(delta):
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed and
 	    self.get_parent().wood):
-		if (self.animator.get_current_animation() !=
-		    self.night_fire_anims[-1]):
+		if self.animator.get_current_animation() != self.night_anims[-1]:
 			self.get_parent().wood = self.get_parent().wood - 1
 			self.kindle()
 
 func night_comes():
-	self.animator.play(night_fire_anims[0])
+	self.animator.play(night_anims[0])
 
 func day_comes():
 	self.animator.stop()
 	self.fire_sprite.set_texture(self.day_fire)
 
 func kindle():
-	var idx = night_fire_anims.find(animator.get_current_animation())
-	if idx + 1 < night_fire_anims.size():
-		self.animator.play(self.night_fire_anims[idx + 1])
+	var idx = night_anims.find(animator.get_current_animation())
+	if idx + 1 < night_anims.size():
+		self.animator.play(self.night_anims[idx + 1])
 	else:
 		print("Fire at max")
 	self.since_last_kindle = 0
 
 func unkindle():
-	var idx = night_fire_anims.find(animator.get_current_animation())
-	if (idx > 0 and
-	    self.since_last_kindle >= night_fire_timeouts[idx]):
-		self.animator.play(self.night_fire_anims[idx - 1])
+	var idx = self.night_anims.find(self.animator.get_current_animation())
+	if (idx > 0 and self.since_last_kindle >= self.night_timeouts[idx]):
+		self.animator.play(self.night_anims[idx - 1])
