90161a365e04fdebcde5fbbe098ad2a0872d97fa
NoisyBass <laura.dcs4@gmail.com>
2017-08-01 00:40:16 +0200

    Fixes

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready():
 	get_node("Sprite 2/AnimationPlayer").play("credits")
+
+
+func _on_TextureButton_pressed():
+	get_node("/root/Global").goto_scene("res://Scenes/Menu.tscn")
