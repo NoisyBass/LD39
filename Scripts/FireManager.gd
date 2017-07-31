abaf5fb72bc2f725463d898c0376c3bb5e7b0e97
AlvarBer <alvaro.garcia95@hotmail.com>
2017-07-31 12:19:11 +0100

    Fire now works

--- Scripts/FireManager.gd
+++ Scripts/FireManager.gd
@@ -1,50 +1,53 @@
 extends Node2D
 
+
 export(Texture) var day_fire
 export var night_anims = ["Low Fire", "Medium Fire",
-                               "High Fire", "Max Fire"]
+                          "High Fire", "Max Fire"]
 export var night_timeouts = [60, 40, 30, 15]
 
 onready var animator = get_node("AnimationPlayer")
 onready var fire_sprite = get_node("Sprite")
+onready var light = get_node("Light2D")
 var since_last_kindle
 
 func _ready():
 	set_process(true)
-	self.day_comes()
-	self.since_last_kindle = 0
+	day_comes()
+	since_last_kindle = 0
 
 func _process(delta):
-	self.since_last_kindle += delta
-	self.unkindle()
+	since_last_kindle += delta
+	unkindle()
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed and
-	    self.get_parent().wood and
-	    self.animator.get_current_animation() != self.night_anims[-1]):
-			self.get_parent().wood -= 1
-			self.kindle()
+	    get_parent().wood and
+	    animator.get_current_animation() != night_anims[-1]):
+			get_parent().wood -= 1
+			kindle()
 
 func night_comes():
-	self.animator.play(night_anims[0])
-	var light = get_node("Light2D")
+	animator.play(night_anims[0])
 	light.set_scale(Vector2(1, 1))
 	light.show()
 
 func day_comes():
-	self.animator.stop()
-	self.fire_sprite.set_texture(self.day_fire)
-	get_node("Light2D").hide()
+	animator.stop()
+	fire_sprite.set_texture(day_fire)
+	light.hide()
 
 func kindle():
 	var idx = night_anims.find(animator.get_current_animation())
 	if idx + 1 < night_anims.size():
-		self.animator.play(self.night_anims[idx + 1])
+		animator.play(night_anims[idx + 1])
+		light.scale(Vector2(1.2, 1.2))
 	else:
 		print("Fire at max")
-	self.since_last_kindle = 0
+	since_last_kindle = 0
 
 func unkindle():
-	var idx = self.night_anims.find(self.animator.get_current_animation())
-	if idx > 0 and self.since_last_kindle >= self.night_timeouts[idx]:
-		self.animator.play(self.night_anims[idx - 1])
+	var idx = night_anims.find(animator.get_current_animation())
+	if idx > 0 and since_last_kindle >= night_timeouts[idx]:
+		animator.play(night_anims[idx - 1])
+		light.scale(Vector2(0.8, 0.8))
